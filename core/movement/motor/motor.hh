2821ed6534cb4e8c52153d3e60fc327acbf9a545
Abdullah Elqaq <abdullah@radiirobotics.com>
2020-10-31 06:47:51 -0700

    Removed namespacing and fixed compile issues

--- core/movement/motor/motor.hh
+++ core/movement/motor/motor.hh
@@ -1,35 +1,30 @@
 #pragma once
 
-namespace core::movement
+struct MotorDriverPinSet
 {
+    int pwm;
+    int forward;
+    int backward;
+};
 
-    struct MotorDriverPinSet
-    {
-        int pwm;
-        int forward;
-        int backward;
-    };
+enum MotorDirection
+{
+    STOP,
+    FORWARD,
+    BACKWARD
+};
 
-    enum MotorDirection
-    {
-        STOP,
-        FORWARD,
-        BACKWARD
-    };
+class Motor
+{
+public:
+    Motor(const int &enable_pin, const int &forward_pin, const int &backward_pin);
+    void set_throttle(int pwm_value, MotorDirection dir);
+    int get_rpm();
 
-    class Motor
-    {
-    public:
-        Motor(const int &enable_pin, const int &forward_pin, const int &backward_pin);
-        void set_throttle(int pwm_value, MotorDirection dir);
-        int get_rpm();
+private:
+    MotorDriverPinSet m_pins{};
+    MotorDirection m_dir;
 
-    private:
-        MotorDriverPinSet m_pins{};
-        MotorDirection m_dir;
-
-        int m_rpm;
-        int m_pwm;
-    };
-
-} // namespace core::movement
+    int m_rpm;
+    int m_pwm;
+};
