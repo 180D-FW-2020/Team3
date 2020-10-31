530429011f8ba115bda090797410a19b392e79b6
Abdullah Elqaq <abdullah@radiirobotics.com>
2020-10-31 06:53:51 -0700

    Removed namespacing

--- core/sensing/sensor.hh
+++ core/sensing/sensor.hh
@@ -1,32 +1,28 @@
 #pragma once
 
-namespace core::sensing
+template <class SensorDataType>
+class Sensor
 {
-    template <class SensorDataType>
-    class Sensor
+public:
+    Sensor(const int &pin)
     {
-    public:
-        Sensor(const int &pin)
-        {
-            m_pin = pin;
-        }
+        m_pin = pin;
+    }
 
-        virtual void read_sensor() = 0; // implemented differently for each sensor
+    virtual void read_sensor() = 0; // implemented differently for each sensor
 
-        SensorDataType get_raw_value() const
-        {
-            return m_raw_value;
-        }
+    SensorDataType get_raw_value() const
+    {
+        return m_raw_value;
+    }
 
-        SensorDataType get_sensor_value() const
-        {
-            return m_sensor_value;
-        }
+    SensorDataType get_sensor_value() const
+    {
+        return m_sensor_value;
+    }
 
-    private:
-        SensorDataType m_raw_value;
-        SensorDataType m_sensor_value;
-        int m_pin;
-    };
-
-} // namespace core::sensing
+private:
+    SensorDataType m_raw_value;
+    SensorDataType m_sensor_value;
+    int m_pin;
+};
