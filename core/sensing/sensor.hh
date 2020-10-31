09cba1d33b3d82c7d58ba2244300f551de81b276
Abdullah Elqaq <abdullah@radiirobotics.com>
2020-10-31 06:36:31 -0700

    Added base sensor abstract class with Bazel build target

---
+++ core/sensing/sensor.hh
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace core::sensing
+{
+    template <class SensorDataType>
+    class Sensor
+    {
+    public:
+        Sensor(const int &pin)
+        {
+            m_pin = pin;
+        }
+
+        virtual void read_sensor() = 0; // implemented differently for each sensor
+
+        SensorDataType get_raw_value() const
+        {
+            return m_raw_value;
+        }
+
+        SensorDataType get_sensor_value() const
+        {
+            return m_sensor_value;
+        }
+
+    private:
+        SensorDataType m_raw_value;
+        SensorDataType m_sensor_value;
+        int m_pin;
+    };
+
+} // namespace core::sensing
