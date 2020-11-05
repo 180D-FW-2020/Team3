ce559dd392093b1ebd60d0293f7b1c26223aa09e
Abdullah Elqaq <abdullahelqaq@gmail.com>
2020-11-05 13:21:36 -0800

    [sensors] Added abstract sensor class, RPM sensor class, Bazel files for AVR toolchain (#5)

---
+++ core/sensing/rpm.cc
@@ -0,0 +1,18 @@
+#include "rpm.hh"
+
+void RpmSensor::read_sensor()
+{
+    auto time_diff = micros() - m_state.last_refresh_micros;
+    m_sensor_value.rpm = m_state.ticks * 60000000l / time_diff;
+
+    if (micros() - m_state.last_refresh_micros > 500000l)
+    {
+        m_state.ticks = 0;
+        m_state.last_refresh_micros = micros();
+    }
+}
+
+void RpmSensor::interrupt_handler()
+{
+    m_state.ticks += 1;
+}
