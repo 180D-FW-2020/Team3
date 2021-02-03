08b68a8cc2b31e5c09af251b0b293a54e876eff0
benjaminruiz99 <benjaminruiz@g.ucla.edu>
2021-02-02 20:00:45 -0800

    Added files and functions for target detection

---
+++ controller/contour_recognition.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+from shaperecognition.shapedetector import ShapeDetector
+from shaperecognition.colorlabeler import ColorLabeler
+import imutils
+
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened():
+    rval, frame = vc.read()
+else:
+    rval = False
+
+show_frame = 1
+
+
+def target_recognition(image):
+    targets = list()
+    kernel = np.ones((5,5),np.uint8)
+
+    #resized = imutils.resize(image, width=300)
+    resized = image
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    lab = cv2.cvtColor(resized, cv2.COLOR_BGR2LAB)
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    #blurred = gray
+    edges = cv2.Canny(blurred,50,200)
+    dilated = cv2.dilate(edges, kernel, iterations = 1)
+    cv2.imshow("edges", edges)
+    cv2.imshow("dilated", dilated)
+    # gray = resized
+
+    ratio = image.shape[0] / float(edges.shape[0])
+    cnts = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    sd = ShapeDetector()
+    cl = ColorLabeler()
+    if len(cnts) > 0:
+        #print(str(cnts) + " circles/n")
+        for c in cnts:
+            M = cv2.moments(c)
+            if M["m00"] != 0:
+                cX = int((M["m10"] / M["m00"]) * ratio)
+                cY = int((M["m01"] / M["m00"]) * ratio)
+                c_target = sd.detect(c)
+                shape = c_target[0]
+                coordinates = c_target[1]
+                c = c.astype("float")
+                c *= ratio
+                c = c.astype("int")
+                if shape == "triangle":
+                    if cv2.contourArea(c) > 600:
+                        color = cl.label(lab, c)
+                        targets.append(c_target)
+                        text = "{} {}".format(color, shape)
+                        coords = np.array([np.array(coordinates[0]), np.array(coordinates[1]), np.array(coordinates[2])])
+                        cv2.drawContours(image, [coords.astype(int)], -1, (0, 255, 0), 2)
+                        cv2.putText(image, text, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    
+    cv2.imshow("labels", image)
+    return targets
+
+while rval:
+    rval, image = vc.read()
+    target_recognition(image)
+
+    key = cv2.waitKey(20)
+    if key == 27:
+        break
---
+++ controller/shaperecognition/shapedetector.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+class ShapeDetector:
+    def __init__(self):
+        pass
+
+    def detect(self, c):
+        shape = "unidentified"
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+        coords = np.zeros((3, 2))
+        target_info = []
+        target_info.append(shape)
+        target_info.append(coords)
+        if len(approx) == 3:
+            coords = np.zeros((3, 2))
+            #n = approx.ravel()
+            #i = 0
+            #for j in n:
+            #    if (i % 2 == 0):
+            #        #x = n[i]
+            #        #y = n[i+1]
+            #        coords[i] = (np.array(n[i], n[i+1]))
+            coords[0] = approx[0]
+            coords[1] = approx[1]
+            coords[2] = approx[2]
+            len_1 = np.sqrt(((coords[0, 0]-coords[1, 0])**2)+((coords[0, 1]-coords[1, 1])**2))
+            len_2 = np.sqrt(((coords[0, 0]-coords[2, 0])**2)+((coords[0, 1]-coords[2, 1])**2))
+            #len_3 = np.sqrt(((coords[1, 0]-coords[2, 0])**2)+((coords[1, 1]-coords[2, 1])**2))
+            error_margin = .1*min(len_1, len_2)
+            if ((np.abs(len_1-len_2) < error_margin)):
+                shape = "triangle"
+                target_info[0] = shape
+                target_info[1] = coords
+            #shape = "triangle"
+
+        elif len(approx) == 4:
+            (x, y, w, h) = cv2.boundingRect(approx)
+            ar = w / float(h)
+
+            if ar >= 0.95 and ar <= 1.05:
+                shape = "square" 
+            else:
+                 shape = "rectangle"
+
+        elif len(approx) == 5:
+            shape = "pentagon"
+        elif len(approx) > 5:
+            shape = "something weird"
+            #elif len(approx) == 2:
+            #    shape = "line"
+        else:
+            shape = "circle"
+        return target_info
+        #return shape
+        
