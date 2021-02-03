68ddf75eb9101f47aa325c12f3ed2498b41e3be2
therealhuy <32783643+therealhuy@users.noreply.github.com>
2021-02-02 20:50:57 -0800

    [gm] Added voice control (color)
    Added color keywords to voice unlock file. File needs to be renamed in future. Modified phrase time limit to make it more robust (trade off with latency).

--- controller/voice_unlock.py
+++ controller/voice_unlock.py
@@ -5,14 +5,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from comms.mqtt import interface as mqtt_interface
 import speech_recognition as sr
 
-def start_listening(keyword, callback):
+def start_listening(keywords, callback):
     # obtain audio from the microphone
     r = sr.Recognizer()
 
     while True:
-        with sr.Microphone(device_index=6) as source:
+        with sr.Microphone() as source:
             try:
-                audio = r.listen(source, timeout=2, phrase_time_limit=2)
+                print("Listening")
+                audio = r.listen(source, phrase_time_limit=4)
             except sr.WaitTimeoutError:
                 continue
         # recognize speech using Google Speech Recognition
@@ -21,8 +22,10 @@ def start_listening(keyword, callback):
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
             result = r.recognize_google(audio)
-            if keyword in result:
-                callback()
+            print(result)
+            for keyword in keywords:
+                if keyword in result:
+                    callback(keyword)
 
         except sr.UnknownValueError:
             pass
@@ -30,12 +33,21 @@ def start_listening(keyword, callback):
             print(
                 "Could not request results from Google Speech Recognition service; {0}".format(e))
 
-def default_callback():
-    print("Sending unlock command..")
-    mqtt_manager.send_message("storage_control", "unlock0")
+def default_callback(keyword):
+    valid_colors = ["red", "blue"]
+    if keyword == "unlock":
+        print("Sending unlock command..")
+        mqtt_manager.send_message("storage_control", "unlock0")
+    if keyword in valid_colors:
+        print("Sending color..")
+        mqtt_manager.send_message("target_color", keyword)
+    if keyword == "reset" or keyword == "restart":
+        print("Sending 'All'..")
+        mqtt_manager.send_message("target_color", "All")
 
 mqtt_id = "voice_unlock"
 mqtt_manager = mqtt_interface.MqttInterface(id=mqtt_id, targets=[], topics=[])
 mqtt_manager.start_reading()
 
-start_listening("unlock", default_callback)
+keywords = ["unlock", "red", "blue", "restart", "reset"]
+start_listening(keywords, default_callback)
