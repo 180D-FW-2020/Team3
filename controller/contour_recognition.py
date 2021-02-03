0d5ed34ed0efa9286b778114fb86d1518058362e
benjaminruiz99 <benjaminruiz@g.ucla.edu>
2021-02-03 01:14:51 -0800

    Changed implementation of target detection

--- controller/contour_recognition.py
+++ controller/contour_recognition.py
@@ -43,3 +43,75 @@ def target_recognition(image):
                         #cv2.drawContours(image, [coords.astype(int)], -1, (0, 255, 0), 2)
                         #cv2.putText(image, text, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     return targets
+
+def target_recognition_2(image):
+    targets = list()
+    kernel = np.ones((5,5),np.uint8)
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+
+    kLowerGreen = np.array([40, 40, 40])
+    kUpperGreen = np.array([90, 255, 255])
+
+    #kLowerGreen = np.array([300, 70, 40])
+    #kUpperGreen = np.array([360, 255, 255])
+
+    kLowerBlue = np.array([90, 90, 0])
+    kUpperBlue = np.array([150, 255, 255])
+
+    kLowerRed = np.array([136, 87, 90])
+    kUpperRed = np.array([180, 255, 255])
+
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    color_mask = cv2.inRange(hsv_img, kLowerRed,  kUpperRed)
+    post_mask = cv2.bitwise_and(image, image, mask=color_mask)
+
+    grey_img = cv2.cvtColor(post_mask, cv2.COLOR_BGR2GRAY)
+    blurred_img = cv2.GaussianBlur(grey_img, (7, 7), 0)
+    #blurred_img = grey_img
+    edges = cv2.Canny(blurred_img,50,200)
+    dilated = cv2.dilate(edges, kernel, iterations = 1)
+    #ret, thresh = cv2.threshold(blurred_img.astype('uint8'), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    thresh = dilated
+    thresh = grey_img
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cv2.imshow("blurred", blurred_img)
+    cv2.imshow("thresh", thresh)
+    shape_detector = ShapeDetector()
+    color_labeler = ColorLabeler()
+    ratio = image.shape[0] / float(image.shape[0])
+    if len(cnts) > 0:
+        for c in cnts:
+            M = cv2.moments(c)
+            if M["m00"] != 0:
+                cX = int((M["m10"] / M["m00"]) * ratio)
+                cY = int((M["m01"] / M["m00"]) * ratio)
+                c_target = shape_detector.detect(c)
+                shape = c_target[0]
+                coordinates = c_target[1]
+                c = c.astype("float")
+                c *= ratio
+                c = c.astype("int")
+                #cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                if shape == "triangle":
+                    if cv2.contourArea(c) > 100:
+                        color = color_labeler.label(lab, c)
+                        c_target[0] = color
+                        targets.append(c_target)
+                        text = "{} {}".format(color, shape)
+                        coords = np.array([np.array(coordinates[0]), np.array(coordinates[1]), np.array(coordinates[2])])
+                        cv2.drawContours(image, [coords.astype(int)], -1, (0, 255, 0), 2)
+                        cv2.putText(image, text, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cv2.imshow("image", image)
+    return targets
+
+vid = cv2.VideoCapture(0) 
+
+ret, frame = vid.read() 
+
+while ret is True:
+    ret, frame = vid.read()
+    target_recognition_2(frame)
+    #cv2.imshow("frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
--- controller/shape_recognition/shape_detector.py
+++ controller/shape_recognition/shape_detector.py
@@ -8,7 +8,7 @@ class ShapeDetector:
     def detect(self, c):
         shape = "unidentified"
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+        approx = cv2.approxPolyDP(c, 0.2 * peri, True)
         coords = np.zeros((3, 2))
         target_info = []
         target_info.append(shape)
@@ -21,10 +21,12 @@ class ShapeDetector:
             len_1 = np.sqrt(((coords[0, 0]-coords[1, 0])**2)+((coords[0, 1]-coords[1, 1])**2))
             len_2 = np.sqrt(((coords[0, 0]-coords[2, 0])**2)+((coords[0, 1]-coords[2, 1])**2))
             #len_3 = np.sqrt(((coords[1, 0]-coords[2, 0])**2)+((coords[1, 1]-coords[2, 1])**2))
-            error_margin = .1*min(len_1, len_2)
-            if ((np.abs(len_1-len_2) < error_margin)):
-                shape = "triangle"
-                target_info[0] = shape
-                target_info[1] = coords
-            #shape = "triangle"
+            #error_margin = .1*min(len_1, len_2)
+            #if ((np.abs(len_1-len_2) < error_margin)):
+            #    shape = "triangle"
+            #    target_info[0] = shape
+            #    target_info[1] = coords
+            shape = "triangle"
+            target_info[0] = shape
+            target_info[1] = coords
         return target_info       
