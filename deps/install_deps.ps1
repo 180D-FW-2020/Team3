e9bc8f8de292fdb37972739ea6f1753dd38b5e67
benjaminruiz99 <67084959+benjaminruiz99@users.noreply.github.com>
2021-02-24 16:47:00 -0800

    Added scripts for installing necessary dependecies for Mac and Window… (#30)
    * Added scripts for installing necessary dependecies for Mac and Windows users
    
    * Added newlines
    
    * Changed video source in main module of vision
    
    * Removed a comment

---
+++ deps/install_deps.ps1
@@ -0,0 +1,10 @@
+echo "Installing WALL-U Dependencies"
+pip3 install opencv-contrib-python
+pip install imutils
+pip install SpeechRecognition
+pip install pyaudio
+pip install protobuf 
+pip install pynput
+pip install paho-mqtt
+pip install imagezmq
+pip install numpy
---
+++ deps/install_deps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo "Installing WALL-U Dependencies"
+pip install opencv-contrib-python
+pip install imutils
+pip installl SpeechRecognition
+pip install pyaudio
+pip install protobuf 
+pip install pynput
+pip install paho-mqtt
+pip install imagezmq
+pip install numpy
